c2b9d86965fdce4bf5138e4ef4c4f509b7595d66
Kříž František <ps.kriz.fra@student.sostp.cz>
2022-09-30 13:35:37 +0200

    struktury

---
+++ struktury_konstruktor/Source.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <string>
+#include <object>
+
+using namespace std;
+
+struct Osoba {
+	string jmeno;
+	int vek;
+	Osoba(){}
+	Osoba(string jm, int v) {
+		jmeno = jm;
+		vek = v;
+	}
+};
+
+int main() {
+	int cislo = 12;
+	const int KONSTANTA = 10;
+
+	string jme;
+	int ve;
+
+	Osoba osoby[KONSTANTA];
+
+	for (int i = 0; i < KONSTANTA; i++) {
+		cout << "Zadej jmeno: ";
+		cin >> jme;
+		cout << "Zadej vek: ";
+		cin >> ve;
+		osoby[i] = Osoba(jme, ve);
+	}
+
+	for (int i = 0; i < KONSTANTA; i++) {
+		cout << "jmeno: " << osoby[i].jmeno << ", vek: " << osoby[i].vek << endl;
+	}
+
+	//string jme;
+	//int ve;
+	//cout << "Zadej jmeno: ";
+	//cin >> jme;
+	//cout << "Zadej vek: ";
+	//cin >> ve;
+
+	//Osoba osoba(jme, ve);
+	////osoba.jmeno = "Tomas";
+	////osoba.vek = 20;
+
+	//cout << "jmeno: " << osoba.jmeno << ", vek: " << osoba.vek << endl;
+
+	system("pause");
+	return 0;
+}
