c3cbf98eb36098ae826e841ae747fb6a9684af7b
František Kříž <thenanoslav@gmail.com>
2023-10-03 10:34:07 +0200

    metody

---
+++ CTVRTAK/metody/Main.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using metody;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("===========================================");
+        Metody.Radek();
+        Console.WriteLine("===========================================");
+        Metody.Radek(5);
+        Console.WriteLine("===========================================");
+        Console.WriteLine("Obsah čtverce o straně a=10 je: " + ObsahyObvody.ObsahCtverce(10));
+        Console.WriteLine("===========================================");
+        Console.WriteLine("Obvod kruhu o poloměru r=5 je: " + ObsahyObvody.ObvodKruhu(5));
+        Console.WriteLine("===========================================");
+        int velikostPole = 10;
+        int[] ints = MojeMetody.NaplnPole(velikostPole);
+        MojeMetody.VypisPole(ints);
+        Console.WriteLine("===========================================");
+
+
+        MojeMetody.Konec();
+
+    }
+}
---
+++ CTVRTAK/metody/MojeMetody.cs
@@ -0,0 +1,71 @@
+﻿using System;
+
+namespace metody
+{
+
+    class MojeMetody
+    {
+        public static int[] NaplnPole(int velikost)
+        {
+            int[] pole = new int[velikost];
+            Random rnd = new Random();
+            for (int i = 0; i < pole.Length; i++)
+            {
+                pole[i] = rnd.Next(1, 100);
+            }
+            return pole;
+        }
+
+        public static int[] VypisPole(int[] pole)
+        {
+            for (int i = 0; i < pole.Length; i++)
+            {
+                Console.WriteLine("pole[" + i + "] = " + pole[i]);
+            }
+            return pole;
+        }
+
+        //metoda ktera skonci program po kliknuti enteru
+        public static void Konec()
+        {
+            Console.WriteLine("Stiskněte Enter pro ukončení programu.");
+
+            while(Console.ReadKey().Key != ConsoleKey.Enter)
+            {
+                Console.Write("\b \b");
+            }
+        }
+
+    }
+
+    class Metody
+    {
+        public static void Radek()
+        {
+            Console.WriteLine();
+        }
+
+        public static void Radek(int pocet)
+        {
+            for (int i = 0; i < pocet; i++)
+            {
+                Console.WriteLine();
+            }
+        }
+
+    }
+
+    class ObsahyObvody
+    {
+        public static double ObsahCtverce(double strA)
+        {
+            return Math.Pow(strA, 2);
+        }
+
+        public static double ObvodKruhu(double polomer)
+        {
+            return Math.Round(2 * Math.PI * polomer, 2);
+        }
+    }
+
+}
