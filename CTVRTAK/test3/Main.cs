aa8d0a2a20c7403d7fdf5a1021ce2a55552eb3cd
František Kříž <thenanoslav@gmail.com>
2023-10-17 09:12:55 +0200

    test3

---
+++ CTVRTAK/test3/Main.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Metody;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Program Bytovy dům");
+        Mistnost[] mistnosti = Vlastnosti.NaplnMistnosti();
+        Console.WriteLine("Plocha vsech mistnosti je " + Vlastnosti.Plocha(mistnosti));
+        Console.ReadLine();
+    }
+}
---
+++ CTVRTAK/test3/Metody.cs
@@ -0,0 +1,74 @@
+﻿using System;
+
+namespace Metody
+{
+
+    class Mistnost
+    {
+        private int sirka;
+        private int delka;
+
+        public Mistnost(int sirka, int delka)
+        {
+            this.sirka = sirka;
+            this.delka = delka;
+        }
+
+        public int getSirka()
+        {
+            return sirka;
+        }
+
+        public int getDelka()
+        {
+            return delka;
+        }
+
+    }
+    
+    class Vlastnosti
+    {
+
+        public static Mistnost[] NaplnMistnosti()
+        {
+            Console.Write("Zadej pocet mistnosti: ");
+            int pocet = int.Parse(Console.ReadLine());
+
+            Mistnost[] mistnosti = new Mistnost[pocet];
+
+            int sirka, delka;
+
+            for(int i = 0; i < pocet; i++)
+            {
+                Console.Clear();
+                Console.WriteLine(i + 1 + ". mistnost");
+                Console.WriteLine("==================");
+                Console.Write("Zadej sirku: ");
+                sirka = int.Parse(Console.ReadLine());
+                Console.Write("Zadej delku: ");
+                delka = int.Parse(Console.ReadLine());
+                mistnosti[i] = new Mistnost(sirka, delka);
+            }
+
+            return mistnosti;
+
+        }
+
+        public static int Plocha(Mistnost[] mistnosti)
+        {
+
+            Console.Clear();
+
+            int plocha = 0;
+
+            for (int i = 0; i < mistnosti.Length; i++)
+            {
+                plocha += (mistnosti[i].getSirka() * mistnosti[i].getDelka());
+            }
+
+            return plocha;
+        }
+
+    }
+
+}
