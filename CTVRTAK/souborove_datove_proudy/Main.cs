00c2ec92a7e56bcdf06dc86465e49ef4df190038
František Kříž <thenanoslav@gmail.com>
2023-10-24 10:22:56 +0200

    souborove datove proudy

---
+++ CTVRTAK/souborove_datove_proudy/Main.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+using System.IO;
+
+class Program
+{
+    static void Main()
+    {
+        //======================================================================
+        //                        Zapis do souboru
+        //======================================================================
+        StreamWriter zapis = new StreamWriter("soubor.txt");
+
+        zapis.WriteLine("Ahoj svete!");
+        zapis.WriteLine("Jak se mas?");
+        zapis.WriteLine("Ja se mam dobre.");
+
+        //zapis.Close(); // Uzavreni souboru - zapis do souboru je ukoncen
+        //zapis.Flush(); // Vyprazdneni bufferu - mohu znovu zapisovat
+
+        zapis.WriteLine("xxAhoj svete!");
+        zapis.WriteLine("xxJak se mas?");
+        zapis.WriteLine("xxJa se mam dobre.");
+
+        zapis.Close();
+
+        //======================================================================
+        //                        Cteni ze souboru
+        //======================================================================
+        StreamReader cteni = new StreamReader("data.txt");
+        //string text = cteni.ReadToEnd();
+
+        Osoba[] osoby = new Osoba[100];
+
+        string radek;
+        int pocet = 0;
+        while ((radek = cteni.ReadLine()) != null)
+        {
+            string[] pole = radek.Split(' ');
+            Console.WriteLine(pole[0] + " " + pole[1]);
+            osoby[pocet] = new Osoba(pole[0], int.Parse(pole[1]), pole[2]);
+
+            pocet++;
+        }
+
+        cteni.Close();
+
+        Console.WriteLine("======================================");
+        Console.WriteLine("               Výpis                  ");
+        Console.WriteLine("======================================");
+
+        double suma = 0;
+        for (int i = 0; i < pocet; i++)
+        {
+            Console.WriteLine($"{i + 1}. {osoby[i].Jmeno} {osoby[i].Vek} {osoby[i].Mesto}");
+            suma += osoby[i].Vek;
+        }
+
+        Console.WriteLine("======================================");
+        Console.WriteLine($"Prumerny vek: {suma / pocet}");
+
+        Console.ReadKey();
+    }
+}
+//======================================================================
+class Osoba
+{
+    public string Jmeno { get; }
+    public int Vek { get; }
+    public string Mesto { get; }
+
+    public Osoba(string jmeno, int vek, string mesto)
+    {
+        Jmeno = jmeno;
+        Vek = vek;
+        Mesto = mesto;
+    }
+}
---
+++ CTVRTAK/string/Main.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Text;
+
+class Program
+{
+    static void Main()
+    {
+
+        string text = "ahoj jak se vede";
+
+        for (int i = 0; i < text.Length; i++)
+        {
+            if (text[i] == 'a')
+            {
+                //text[i] = 'b'; // nefunguje
+            }
+        }
+
+        StringBuilder sb = new StringBuilder();
+        sb.Append(text);
+
+        for (int i = 0; i < sb.Length; i++)
+        {
+            if (sb[i] == 'a')
+            {
+                sb[i] = 'b';
+            }
+        }
+
+        Console.WriteLine(sb);
+        //======================================================================
+        string text2 = "ahoj";
+        text2 += " cau" + " nazdar";
+
+        Console.WriteLine(text2);
+
+        Console.WriteLine(text2.Insert(5, "hi "));
+        Console.WriteLine(text2);
+
+        //======================================================================
+
+        string veta = "ahoj jak se    vede;třído";
+        //string[] pole = veta.Split(' ');
+        char[] znaky = new char[] { ' ', ';', '#', '@' };
+        string[] pole = veta.Split(znaky, StringSplitOptions.RemoveEmptyEntries);
+        //string[] pole = veta.Split(new char[] {' ', ';', '#', '@'});
+
+        for (int i = 0; i < pole.Length; i++)
+        {
+            Console.WriteLine(pole[i]);
+        }
+
+        //======================================================================
+
+        string spojeni = string.Join(" ", pole);
+
+        Console.WriteLine(spojeni);
+
+        Console.ReadKey();
+    }
+}
